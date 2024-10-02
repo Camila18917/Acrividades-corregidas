034129dd781492c8f83a0881bc120ad401470808
Camila18917 <camilagomez18917@gmail.com>
2024-10-01 23:46:37 -0300

    Actualizaciones

--- Condiciones-corregido/3/script.js
+++ Condiciones-corregido/3/script.js
@@ -1,21 +1,16 @@
-function comprobarFecha() {
-    event.preventDefault();
+function comprobarfecha() {
+    event.preventDefault(); 
     const fechaSeleccionada = new Date(document.getElementById('fecha').value);
     const fechaActual = new Date();
-    // Ajustamos la fecha actual a solo el día (sin hora)
-    fechaActual.setHours(0, 0, 0, 0);
-
+    fechaActual.setHours(0, 0, 0, 0); 
     const resultadoDiv = document.getElementById('resultado');
 
     if (!document.getElementById('fecha').value) {
         resultadoDiv.textContent = "Por favor, selecciona una fecha.";
-        resultadoDiv.style.color = "orange";
+        resultadoDiv.style.color = "rgb(136, 120, 105)";
     } else if (fechaSeleccionada < fechaActual) {
         resultadoDiv.textContent = "La fecha seleccionada ya ha pasado.";
         resultadoDiv.style.color = "red";
-    } else if (fechaSeleccionada.getTime() === fechaActual.getTime()) {
-        resultadoDiv.textContent = "La fecha seleccionada es hoy.";
-        resultadoDiv.style.color = "blue";
     } else {
         resultadoDiv.textContent = "La fecha seleccionada está disponible.";
         resultadoDiv.style.color = "green";
